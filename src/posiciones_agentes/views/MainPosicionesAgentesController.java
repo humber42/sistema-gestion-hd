80953a01e1e15c1fa6e510326be9e2fc3d78b95c
henry1806 <henry1806@nauta.cu>
2021-09-01 12:20:34 -0400

    Actualizando modulo de posiciones agentes: - Servicios Implementados - Edicion de tarifa implementada

--- src/posiciones_agentes/views/MainPosicionesAgentesController.java
+++ src/posiciones_agentes/views/MainPosicionesAgentesController.java
@@ -1,8 +1,15 @@
 package posiciones_agentes.views;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.controlsfx.dialog.ExceptionDialog;
+
+import java.io.IOException;
 
 public class MainPosicionesAgentesController {
     private Stage mainApp;
@@ -28,5 +35,29 @@ public class MainPosicionesAgentesController {
 
     public void setPanelPosicionesAgentes(BorderPane pane) {
         this.panelPosicionesAgentes = pane;
+    }
+
+    @FXML
+    private void editTarifa(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainPosicionesAgentesController.class.getResource("../views/EdicionTarifasView.fxml"));
+            AnchorPane pane = loader.load();
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Editar tarifas");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(this.mainApp);
+            Scene scene = new Scene(pane);
+            dialogStage.setScene(scene);
+
+            EdicionTarifasContoller controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+
+            ExceptionDialog dialog = new ExceptionDialog(e);
+            dialog.showAndWait();
+        }
     }
 }
