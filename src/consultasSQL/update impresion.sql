68c60a97ea25f96832413a6c58b0db8d7f311941
henry1806 <henry1806@nauta.cu>
2021-10-25 15:26:36 -0400

    commit completo con todo lo de esclarecimiento, dar bajas, y lo demás que pidio Humberto padre

--- src/consultasSQL/update impresion.sql
+++ src/consultasSQL/update impresion.sql
@@ -19,3 +19,26 @@ begin
   insert into registro_impresiones (id_reg, cant_impresiones, ultima_impresion) values ($1, 1, fecha);
 end;
 $$;
+
+CREATE OR REPLACE FUNCTION obtener_impresiones()
+  RETURNS SETOF impresiones AS
+$BODY$
+DECLARE rec record;
+BEGIN
+
+  FOR rec IN
+  SELECT numero_pase, numero_identidad, nombre, tipo_pase, substring(codigo,0,2), cant_impresiones
+  FROM registro_pases
+         LEFT JOIN registro_impresiones ON registro_pases.id_reg = registro_impresiones.id_reg
+         JOIN tipos_pase_identificativo ON registro_pases.id_tipo_pase = tipos_pase_identificativo.id
+         JOIN codigo_pase_identificativo ON registro_pases.id_codigo_pase = codigo_pase_identificativo.id
+  WHERE baja = 0
+
+  LOOP
+    RETURN NEXT rec;
+  end loop;
+  RETURN ;
+end;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100
