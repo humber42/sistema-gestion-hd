48751d5223279fed8a5309cb2942d2ff0599fac3
Htico <hcabrera@ceis.cujae.edu.cu>
2021-11-13 01:13:40 -0500

    project finished

--- src/informes_generate/ProcessExcelTPub.java
+++ src/informes_generate/ProcessExcelTPub.java
@@ -1,11 +1,8 @@
 package informes_generate;
 
 import models.TpubCentroAgenteFromExcel;
-import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
-import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -25,12 +22,25 @@ public class ProcessExcelTPub {
             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
             XSSFSheet sheet = workbook.getSheetAt(0);
             Iterator<Row> rowIterator = sheet.iterator();
+            int cantRows = 0;
             while(rowIterator.hasNext()){
                 Row row = rowIterator.next();
-                if(row.getRowNum()>3&&!row.getCell(1).getStringCellValue().equalsIgnoreCase("Total")){
-                    System.out.println(row.getCell(1).getStringCellValue());
+                if (row.getRowNum() > 2 && !row.getCell(1).getStringCellValue().contains("TOTAL") && row.getRowNum() < 186) {
+                    String municipio = row.getCell(1).getStringCellValue();
+                    int tpub = Double.valueOf((row.getCell(13)).getNumericCellValue()).intValue();
+                    int centroAgente = Double.valueOf(row.getCell(12).getNumericCellValue()).intValue();
+
+                    System.out.println(municipio + " " + tpub + " " + centroAgente);
+                    System.out.println("-----------");
+                    data.add(new TpubCentroAgenteFromExcel(
+                            municipio,
+                            tpub,
+                            centroAgente
+                    ));
                 }
+                cantRows++;
             }
+            System.out.println(cantRows);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -43,14 +53,78 @@ public class ProcessExcelTPub {
             FileInputStream fileInputStream = new FileInputStream(excelFile);
             HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
             HSSFSheet sheet = workbook.getSheetAt(0);
-            HSSFFormulaEvaluator evaluator = new HSSFFormulaEvaluator(sheet,workbook);
-            Iterator<HSSFRow> rowIterator = sheet.rowIterator();
+            Iterator<Row> rowIterator = sheet.rowIterator();
+            int cantSanLuis = 0;
             while(rowIterator.hasNext()){
-                HSSFRow row = rowIterator.next();
-                String municipio =  row.getCell(Short.parseShort("1")).getStringCellValue();
-                int tpub = Double.valueOf((row.getCell(Short.parseShort("13"))).getNumericCellValue()).intValue();
-                int centroAgente = Double.valueOf(evaluator.evaluate(row.getCell(Short.parseShort("12"))).getNumberValue()).intValue();
-                if(row.getRowNum()>2&&!row.getCell(Short.parseShort("1")).getStringCellValue().equalsIgnoreCase("Total")){
+                Row row = rowIterator.next();
+                if (row.getRowNum() > 2 && !row.getCell(1).getStringCellValue().contains("TOTAL") && row.getRowNum() < 186) {
+                    String municipio = row.getCell(1).getStringCellValue();
+                    if (municipio.equalsIgnoreCase("San Luis") && cantSanLuis < 1) {
+                        cantSanLuis++;
+                        municipio = "San Luis (PR)";
+                    } else if (municipio.equalsIgnoreCase("San Luis") && cantSanLuis > 0) {
+                        cantSanLuis = 0;
+                        municipio = "San Luis (SC)";
+                    } else if (municipio.equalsIgnoreCase("San Juan y Mtnez")) {
+                        municipio = "San Juan y Martínez";
+                    } else if (municipio.equalsIgnoreCase("Alquizar")) {
+                        municipio = "Alquízar";
+                    } else if (municipio.equalsIgnoreCase("Guira de Melena")) {
+                        municipio = "Güira de Melena";
+                    } else if (municipio.equalsIgnoreCase("San Cristobal")) {
+                        municipio = "San Cristóbal";
+                    } else if (municipio.equalsIgnoreCase("Guines")) {
+                        municipio = "Güines";
+                    } else if (municipio.equalsIgnoreCase("10 de Octubre")) {
+                        municipio = "Diez de Octubre";
+                    } else if (municipio.equalsIgnoreCase("Jaguey Grande")) {
+                        municipio = "Jagüey Grande";
+                    } else if (municipio.equalsIgnoreCase("Quemado de Guines")) {
+                        municipio = "Quemado de Güines";
+                    } else if (municipio.equalsIgnoreCase("Cabaiguan")) {
+                        municipio = "Cabaiguán";
+                    } else if (municipio.equalsIgnoreCase("Baragua ")) {
+                        municipio = "Baraguá";
+                    } else if (municipio.equalsIgnoreCase("1ro de Enero")) {
+                        municipio = "Primero de Enero";
+                    } else if (municipio.equalsIgnoreCase("Ciego de Avila")) {
+                        municipio = "Ciego de Ávila";
+                    } else if (municipio.equalsIgnoreCase("Camaguey")) {
+                        municipio = "Camagüey";
+                    } else if (municipio.equalsIgnoreCase("Céspedes")) {
+                        municipio = "Carlos Manuel de Céspedes";
+                    } else if (municipio.equalsIgnoreCase("Guaimaro")) {
+                        municipio = "Guáimaro";
+                    } else if (municipio.equalsIgnoreCase("Gíbara")) {
+                        municipio = "Gibara";
+                    } else if (municipio.equalsIgnoreCase("Pilon")) {
+                        municipio = "Pilón";
+                    } else if (municipio.equalsIgnoreCase("Rio Cauto")) {
+                        municipio = "Río Cauto";
+                    } else if (municipio.equalsIgnoreCase("II Frente")) {
+                        municipio = "Segundo Frente";
+                    } else if (municipio.equalsIgnoreCase("Songo La Maya ")) {
+                        municipio = "Songo - La Maya";
+                    } else if (municipio.equalsIgnoreCase("III Frente")) {
+                        municipio = "Tercer Frente";
+                    } else if (municipio.equalsIgnoreCase("Imias")) {
+                        municipio = "Imías";
+                    } else if (municipio.equalsIgnoreCase("San Antonio de las Baños")) {
+                        municipio = "San Antonio de los Baños";
+                    } else if (municipio.equalsIgnoreCase("Bahia Honda")) {
+                        municipio = "Bahía Honda";
+                    } else if (municipio.equalsIgnoreCase("Sagua La Grande")) {
+                        municipio = "Sagua la Grande";
+                    } else if (municipio.equalsIgnoreCase("Morón ")) {
+                        municipio = "Morón";
+                    } else if (municipio.equalsIgnoreCase("Antillas")) {
+                        municipio = "Antilla";
+                    }
+                    int tpub = Double.valueOf((row.getCell(13)).getNumericCellValue()).intValue();
+                    int centroAgente = Double.valueOf(row.getCell(12).getNumericCellValue()).intValue();
+
+                    System.out.println(municipio + " " + tpub + " " + centroAgente);
+                    System.out.println("-----------");
                     data.add( new TpubCentroAgenteFromExcel(
                            municipio,
                             tpub,
@@ -58,10 +132,7 @@ public class ProcessExcelTPub {
                     ));
                 }
             }
-            for (TpubCentroAgenteFromExcel pp:
-                 data) {
-                System.out.println(pp.getMunicipio()+" "+pp.getCentroAgentes()+" "+pp.getTpubs());
-            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
--- src/views/TelefoniaPublicaViewController.java
+++ src/views/TelefoniaPublicaViewController.java
@@ -5,24 +5,32 @@ import informes_generate.ProcessExcelTPub;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import models.EstacionPublicaCentroAgente;
+import models.Municipio;
+import models.TpubCentroAgenteFromExcel;
 import models.UnidadOrganizativa;
 import org.controlsfx.control.textfield.TextFields;
 import seguridad.models.UserLoggedIn;
 import seguridad.views.LoginViewController;
 import services.ServiceLocator;
 import util.Util;
+import views.dialogs.DialogLoadingController;
+import views.dialogs.DialogLoadingUrl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,6 +57,8 @@ public class TelefoniaPublicaViewController {
     private UserLoggedIn logged;
 
     private Stage dialogStage;
+
+    private Stage dialogLoading;
 
     private MainApp mainApp;
 
@@ -242,13 +252,59 @@ public class TelefoniaPublicaViewController {
                 new FileChooser.ExtensionFilter("Excel Files 97-2003","*.xls")
 
         );
+
+
+        List<TpubCentroAgenteFromExcel> data = new LinkedList<>();
         File fileExcel = fileChooser.showOpenDialog(this.dialogStage);
-        if(fileExcel!=null){
-            if(fileExcel.getPath().contains(".xslx"))
-                ProcessExcelTPub.getDataFromExcel(fileExcel,true);
-            else
-                ProcessExcelTPub.getDataFromExcel(fileExcel);
-        }
+
+        Task<Boolean> task = new Task<Boolean>() {
+            private
+            List<TpubCentroAgenteFromExcel> data;
+
+            @Override
+            protected Boolean call() throws Exception {
+                data = new LinkedList<>();
+                if (fileExcel != null) {
+                    if (fileExcel.getPath().contains(".xslx"))
+                        data = ProcessExcelTPub.getDataFromExcel(fileExcel, true);
+                    else
+                        data = ProcessExcelTPub.getDataFromExcel(fileExcel);
+                }
+                for (TpubCentroAgenteFromExcel tpubEstacion : data) {
+                    String municipioName = tpubEstacion.getMunicipio();
+                    try {
+                        Municipio municipio = ServiceLocator.getMunicipiosService().searchMunicipioByName(tpubEstacion.getMunicipio());
+                        ServiceLocator.getEstacionPublicaCentroAgenteService().updateEstacionPublicaByIdMunicipio(
+                                municipio.getId_municipio(),
+                                tpubEstacion.getCentroAgentes(),
+                                tpubEstacion.getTpubs()
+                        );
+                    } catch (Exception e) {
+                        System.out.println("Error con el municipio: " + municipioName);
+                        e.printStackTrace();
+                    }
+                }
+                return true;
+            }
+
+            @Override
+            protected void succeeded() {
+                super.succeeded();
+                dialogLoading.close();
+                initializeTableTelefonia(ServiceLocator.getEstacionPublicaCentroAgenteService().getAll(), true);
+            }
+
+            @Override
+            protected void failed() {
+                super.failed();
+                dialogLoading.close();
+                Util.dialogResult("Error", Alert.AlertType.ERROR);
+            }
+        };
+
+        Thread th = new Thread(task);
+        loadDialogLoading(this.dialogStage);
+        th.start();
     }
 
     @FXML
@@ -306,5 +362,24 @@ public class TelefoniaPublicaViewController {
 
     public static EstacionPublicaCentroAgente getEstacionPublicaCentroAgenteSelected(){
         return estacionSelected;
+    }
+
+    private void loadDialogLoading(Stage mainApp) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(DialogLoadingUrl.class.getResource("DialogLoading.fxml"));
+            StackPane panel = loader.load();
+            dialogLoading = new Stage();
+            dialogLoading.setScene(new Scene(panel));
+            dialogLoading.initModality(Modality.WINDOW_MODAL);
+            dialogLoading.initOwner(mainApp);
+            dialogLoading.initStyle(StageStyle.UNDECORATED);
+            DialogLoadingController controller = loader.getController();
+            controller.setLabelText("Procesando");
+            //controller.setLabelText("Cargando");
+            dialogLoading.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
